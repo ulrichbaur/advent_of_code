2492917872f8e2d8348880bea98007de8211fe9b
Ulrich Baur <ulrich.baur@mailbox.org>
2023-12-03 21:16:35 +0100

    aoc2023 day 1: part 2 complete

--- 2023/rust/day01-trebuchet/src/main.rs
+++ 2023/rust/day01-trebuchet/src/main.rs
@@ -3,6 +3,7 @@
 // - For every line, combine the first and last digit
 // - Build the sum
 
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
@@ -14,6 +15,7 @@ fn main() -> io::Result<()> {
     for line in reader.lines() {
         let line = line?;
         let number = parse_number_from_string(&line);
+        // println!("{}: {}", line, number);
         sum += number;
     }
 
@@ -22,39 +24,122 @@ fn main() -> io::Result<()> {
 }
 
 fn parse_number_from_string(s: &str) -> u32 {
-    let first_number = s.chars().filter(|c| c.is_numeric()).next();
-    let first_number = match first_number {
-        Some(num) => num.to_digit(10),
-        None => panic!("No number in string"),
-    };
-    let first_number = match first_number {
-        Some(num) => num,
-        None => panic!("Conversion to number failed for first_number"),
-    };
+    let first_number = find_first_digit_in_string(s);
 
-    let last_number = s.chars().rev().filter(|c| c.is_numeric()).next();
-    let last_number = match last_number {
-        Some(num) => num.to_digit(10),
-        None => panic!("No number in string"),
-    };
-    let last_number = match last_number {
-        Some(num) => num,
-        None => panic!("Conversion to number failed for last_number"),
-    };
+    let last_number = find_last_digit_in_string(s);
 
     10 * first_number + last_number
+}
+
+const DIGITS: [&str; 18] = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six",
+    "seven", "eight", "nine",
+];
+fn find_first_digit_in_string(s: &str) -> u32 {
+    let mut map: HashMap<String, u32> = Default::default();
+    for digit in DIGITS.iter() {
+        let index = s.find(digit);
+        match index {
+            Some(num) => map.insert(digit.to_string(), num.try_into().unwrap()),
+            None => continue,
+        };
+    }
+
+    let (key, _) = map.iter().min_by_key(|&(_, v)| v).unwrap();
+
+    match_key_to_digit(key)
+}
+
+fn find_last_digit_in_string(s: &str) -> u32 {
+    let mut map: HashMap<String, u32> = Default::default();
+    for digit in DIGITS.iter() {
+        let index = s.rfind(digit);
+        match index {
+            Some(num) => map.insert(digit.to_string(), num.try_into().unwrap()),
+            None => continue,
+        };
+    }
+
+    let (key, _) = map.iter().max_by_key(|&(_, v)| v).unwrap();
+
+    match_key_to_digit(key)
+}
+
+fn match_key_to_digit(key: &str) -> u32 {
+    let result = match key {
+        "1" | "one" => 1,
+        "2" | "two" => 2,
+        "3" | "three" => 3,
+        "4" | "four" => 4,
+        "5" | "five" => 5,
+        "6" | "six" => 6,
+        "7" | "seven" => 7,
+        "8" | "eight" => 8,
+        "9" | "nine" => 9,
+        _ => panic!("Something else"),
+    };
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn can_parse_number_from_string() {
-        let number = parse_number_from_string("1aa2");
-        assert_eq!(number, 12);
-        let number = parse_number_from_string("bc3aa4de5bad");
-        assert_eq!(number, 35);
-        let number = parse_number_from_string("aa123bbd13152dad313doo");
+    fn can_parse_raw_digits_from_string() {
+        let first_digit = find_first_digit_in_string("123");
+        let last_digit = find_last_digit_in_string("123");
+        assert_eq!(first_digit, 1);
+        assert_eq!(last_digit, 3);
+
+        let first_digit = find_first_digit_in_string("567");
+        let last_digit = find_last_digit_in_string("567");
+        assert_eq!(first_digit, 5);
+        assert_eq!(last_digit, 7);
+    }
+
+    #[test]
+    fn can_parse_digits_as_words_from_string() {
+        let first_digit = find_first_digit_in_string("onetwothree");
+        let last_digit = find_last_digit_in_string("onetwothree");
+        assert_eq!(first_digit, 1);
+        assert_eq!(last_digit, 3);
+
+        let first_digit = find_first_digit_in_string("fivesixseven");
+        let last_digit = find_last_digit_in_string("fivesixseven");
+        assert_eq!(first_digit, 5);
+        assert_eq!(last_digit, 7);
+    }
+    #[test]
+    fn can_parse_mix_of_raw_digits_and_digits_as_words_from_string() {
+        let first_digit = find_first_digit_in_string("onetwo3");
+        let last_digit = find_last_digit_in_string("onetwo3");
+        assert_eq!(first_digit, 1);
+        assert_eq!(last_digit, 3);
+
+        let first_digit = find_first_digit_in_string("5sixseven");
+        let last_digit = find_last_digit_in_string("5sixseven");
+        assert_eq!(first_digit, 5);
+        assert_eq!(last_digit, 7);
+    }
+    #[test]
+    fn can_parse_mix_of_raw_digits_and_digits_as_words_with_added_noise_from_string() {
+        let first_digit = find_first_digit_in_string("sdlonejakdstwo3sadad");
+        let last_digit = find_last_digit_in_string("sdlonejakdstwo3sadad");
+        assert_eq!(first_digit, 1);
+        assert_eq!(last_digit, 3);
+
+        let first_digit = find_first_digit_in_string("bsaf5sixahasdasevenfadf");
+        let last_digit = find_last_digit_in_string("bsaf5sixahasdasevenfadf");
+        assert_eq!(first_digit, 5);
+        assert_eq!(last_digit, 7);
+    }
+
+    #[test]
+    fn can_parse_number_from_mixed_string() {
+        let number = parse_number_from_string("sdlonejakdstwo3sadad");
         assert_eq!(number, 13);
+
+        let number = parse_number_from_string("bsaf5sixahasdasevenfadf");
+        assert_eq!(number, 57);
     }
 }
