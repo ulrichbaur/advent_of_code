67b7e9f197718f14096d1db07d1053a57f2e78f2
Ulrich Baur <ulrich.baur@mailbox.org>
2023-12-04 21:59:28 +0100

    aoc2023 day 2: part 1 complete

---
+++ 2023/rust/day02-cube-conundrum/src/main.rs
@@ -0,0 +1,251 @@
+// Task:
+// Part 1:
+// - Parse a file line by line
+// - game is possible if red <= 12, green <= 13, green <= 14
+// - For every line, one line per game
+//      - split game into turns (separated by ";")
+//      - check if game is possible given every turn
+//          - if game is possible, add game id to sum, then proceed with next line
+//          - if not, go to next line
+// Part 2:
+// - ...
+
+const RED_LIMIT: u32 = 12;
+const GREEN_LIMIT: u32 = 13;
+const BLUE_LIMIT: u32 = 14;
+
+#[derive(Debug, PartialEq, Eq)]
+struct Turn {
+    red: u32,
+    blue: u32,
+    green: u32,
+}
+
+impl Turn {
+    fn is_possible(self: &Self) -> bool {
+        self.red <= RED_LIMIT && self.green <= GREEN_LIMIT && self.blue <= BLUE_LIMIT
+    }
+
+    fn from(text: &str) -> Self {
+        let mut turn = Turn {
+            red: 0,
+            blue: 0,
+            green: 0,
+        };
+        let terms: Vec<&str> = text.split(',').map(|s| s.trim()).collect();
+
+        for term in terms {
+            if term.contains("red") {
+                let red_value: Vec<&str> = term.split("red").map(|s| s.trim()).collect();
+                let value = red_value[0].parse::<u32>();
+                match value {
+                    Ok(result) => turn.red = result,
+                    Err(_) => panic!("something went wrong while parsing red"),
+                }
+            } else if term.contains("green") {
+                let green_value: Vec<&str> = term.split("green").map(|s| s.trim()).collect();
+                let value = green_value[0].parse::<u32>();
+                match value {
+                    Ok(result) => turn.green = result,
+                    Err(_) => panic!("something went wrong while parsing green"),
+                }
+            } else if term.contains("blue") {
+                let blue_value: Vec<&str> = term.split("blue").map(|s| s.trim()).collect();
+                let value = blue_value[0].parse::<u32>();
+                match value {
+                    Ok(result) => turn.blue = result,
+                    Err(_) => panic!("something went wrong while parsing blue"),
+                }
+            }
+        }
+
+        turn
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Game {
+    id: u32,
+    turns: Vec<Turn>,
+}
+
+impl Game {
+    fn is_possible(self: &Self) -> bool {
+        for turn in &self.turns {
+            if !turn.is_possible() {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn from(text: &str) -> Self {
+        let terms: Vec<&str> = text.split(':').map(|s| s.trim()).collect();
+
+        // parse game id
+        let id_terms: Vec<&str> = terms[0].split("Game").map(|s| s.trim()).collect();
+        let game_id = id_terms[1].parse::<u32>();
+        let id = match game_id {
+            Ok(result) => result,
+            Err(_) => panic!("Can't parse id from line"),
+        };
+        // parse turns
+        let mut turns: Vec<Turn> = vec![];
+        let turn_terms: Vec<&str> = terms[1].split(";").map(|s| s.trim()).collect();
+
+        for turn_text in turn_terms {
+            let turn = Turn::from(turn_text);
+            turns.push(turn);
+        }
+
+        Game { id, turns }
+    }
+}
+
+fn main() {
+    let input: Vec<&str> = include_str!("../data/input.txt").lines().collect();
+
+    let turn = Turn::from("5 red, 3 green, 1 blue");
+    println!("{:?}", turn);
+
+    let mut sum = 0;
+    for line in input {
+        let game = Game::from(line);
+        println!("Is Game {} possible? {}", game.id, game.is_possible());
+        if game.is_possible() {
+            sum += game.id;
+        }
+    }
+
+    println!("Sum of possible game ids: {}", sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn possible_turns_are_recognized_as_possible() {
+        let turn = Turn {
+            red: 10,
+            blue: 5,
+            green: 13,
+        };
+        assert!(turn.is_possible());
+    }
+
+    #[test]
+    fn impossible_turns_are_recognized_as_not_possible() {
+        let turn = Turn {
+            red: 10,
+            blue: 16,
+            green: 13,
+        };
+        assert!(!turn.is_possible());
+    }
+
+    #[test]
+    fn game_with_only_possible_turns_is_recognized_as_possible() {
+        let game = Game {
+            id: 1,
+            turns: vec![
+                Turn {
+                    red: 10,
+                    blue: 5,
+                    green: 13,
+                },
+                Turn {
+                    red: 10,
+                    blue: 11,
+                    green: 13,
+                },
+                Turn {
+                    red: 2,
+                    blue: 5,
+                    green: 0,
+                },
+            ],
+        };
+
+        assert!(game.is_possible());
+    }
+
+    #[test]
+    fn game_with_at_least_one_impossible_turn_is_recognized_as_not_possible() {
+        let game = Game {
+            id: 2,
+            turns: vec![
+                Turn {
+                    red: 10,
+                    blue: 5,
+                    green: 13,
+                },
+                Turn {
+                    red: 10,
+                    blue: 11,
+                    green: 19,
+                },
+                Turn {
+                    red: 2,
+                    blue: 5,
+                    green: 0,
+                },
+            ],
+        };
+
+        assert!(!game.is_possible());
+    }
+
+    #[test]
+    fn can_parse_turns_from_text() {
+        // case 1
+        let turn = Turn::from("5 red, 3 green, 1 blue");
+        assert_eq!(
+            turn,
+            Turn {
+                red: 5,
+                green: 3,
+                blue: 1
+            }
+        );
+
+        // case 2
+        let turn = Turn::from("10 blue, 7 green, 1 red");
+        assert_eq!(
+            turn,
+            Turn {
+                red: 1,
+                green: 7,
+                blue: 10
+            }
+        );
+    }
+
+    #[test]
+    fn can_parse_game_from_text() {
+        let game = Game::from("Game 113: 10 blue, 7 green, 1 red; 5 red, 3 green, 1 blue; 10 blue");
+        assert_eq!(
+            game,
+            Game {
+                id: 113,
+                turns: vec![
+                    Turn {
+                        red: 1,
+                        green: 7,
+                        blue: 10
+                    },
+                    Turn {
+                        red: 5,
+                        green: 3,
+                        blue: 1
+                    },
+                    Turn {
+                        red: 0,
+                        green: 0,
+                        blue: 10
+                    }
+                ]
+            }
+        );
+    }
+}
